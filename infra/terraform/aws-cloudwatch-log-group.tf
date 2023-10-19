913154b27c926dbdbaac70da1f323a018af688fc
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-19 16:52:58 -0300

    Implementação da Infra ECS

--- infra/terraform/aws-cloudwatch-log-group.tf
+++ infra/terraform/aws-cloudwatch-log-group.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "mixfast_cloudwatch_log_group" {
-  name = "/aws/ecs/${var.name}-ecs-cluster/cloudwatch/${var.name}"
+  name = "/aws/ecs/${var.name}_ecs_cluster/cloudwatch/${var.name}"
 
   tags = var.tags
 }
